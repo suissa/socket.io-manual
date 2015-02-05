18a723859aa4b0197d1936573ec33cd98b2543ce
Suissa <jnascimento@gmail.com>
2015-02-05 19:29:49 -0200

    Arurmando a casa

--- parte2/namespace.js
+++ parte2/namespace.js
@@ -7,28 +7,13 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/namespace.html');
 });
 
-var nsp1 = io.of('/meu-namespace');
-nsp1.on('connection', function(socket){
-
-  console.log('um usuario conectou');
-
-  socket.on('disconnect', function(){
-    console.log('usuario desconectou');
+var namespace = io
+  .of('/namespace')
+  .on('connection', function(socket) {
+    socket.on('message', function(data) {
+      socket.broadcast.send(data);
+    });
   });
-
-  socket.on('chat message', function(msg){
-    console.log('menssagem: ' + msg);
-    io.emit('chat message', msg);
-  });
-
-})
-
-
-var nsp = io.of('/meu-namespace');
-nsp.on('connection', function(socket){
-  console.log('alguém conectou')
-});
-// nsp.emit('chat message', 'oi para todos!');
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
